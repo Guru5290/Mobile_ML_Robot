779113b35565c003c705c32243e501167e08c0fa
Gareth Kipkoech <garethkip@gmail.com>
2025-10-02 19:23:15 +0300

    Added tasks to pytree

--- pi-code/src/nav_to_poses/nav_to_poses/nav_to_poses.py
+++ pi-code/src/nav_to_poses/nav_to_poses/nav_to_poses.py
@@ -9,6 +9,12 @@ import sys
 import yaml
 import os
 from ament_index_python.packages import get_package_share_directory
+import subprocess, signal
+import time
+from std_msgs.msg import String,Float64MultiArray
+from rclpy.node import Node
+from py_trees.blackboard import Blackboard
+import subprocess
 
 
 
@@ -20,27 +26,187 @@ def print_same_line(msg1, msg2):
 
 
 # ---- Task behaviours ----
-class PrintHello(py_trees.behaviour.Behaviour):
-    def __init__(self, name="PrintHello"):
+
+#Implementing loading and offloading tasks with colour detection
+loaded_colour=None
+class Loading(py_trees.behaviour.Behaviour):
+    def __init__(self, name="Loading"):
         super().__init__(name)
         self.executed = False
+        self.node = None
+        self.colour = None
+        self.process = None
+        self.start_time = None
+        self.timeout = 15  # seconds to run detector
+        self.blackboard = Blackboard()
 
     def update(self):
-        if not self.executed:
-            print("hello")
-            self.executed = True
+        if self.executed:
+            return py_trees.common.Status.SUCCESS
+
+        # Step 1: Start process if not running
+        if self.process is None:
+            # self.process = subprocess.Popen(
+            #     ["ros2", "run", "colour_door_controller", "colour_detector","--ros-args","-p","IP:=10.122.180.67"], #b - Fundi
+            #     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, preexec_fn=os.setsid #start new process group
+            # )
+            self.process = subprocess.Popen(
+                ["ros2", "run", "colour_door_controller", "colour_detector","--ros-args","-p","IP:=10.226.56.67"], #Kabbage - Gareth 
+                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, preexec_fn=os.setsid #start new process group
+            )
+            # self.process = subprocess.Popen(
+            #     ["ros2", "run", "colour_door_controller", "colour_detector","--ros-args","-p","IP:=192.168.0.112"], #Gamefield
+            #     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, preexec_fn=os.setsid #start new process group
+            # )
+            self.start_time = time.time()
+            print(f"{self.name}: Started colour detector for loading...")
+            # time.sleep(5)
+            
+            return py_trees.common.Status.RUNNING
+
+        # Step 2: Wait for timeout to expire
+        if time.time() - self.start_time < self.timeout:
+            return py_trees.common.Status.RUNNING
+
+        # Step 3: Stop detector after timeout
+        self._stop_detector()
+        
+
+        # Take the last detected colour from callback
+        if self.colour:
+            self.blackboard.loading_colour = self.colour
+            global loaded_colour 
+            loaded_colour = self.colour
+            print(f"{self.name}: Stored loaded colour = {loaded_colour}")
+            
+        else:
+            print(f"{self.name}: No colour detected, defaulting to None")
+        
+        self.executed = True     
         return py_trees.common.Status.SUCCESS
 
+    def setup(self, **kwargs):
+        node = kwargs.get("node")
+        if node:
+            self.node = node
+            self.sub = node.create_subscription(
+                String, "/detected_colour", self.colour_callback, 10
+            )
 
-class PrintHi(py_trees.behaviour.Behaviour):
-    def __init__(self, name="PrintHi"):
+    def colour_callback(self, msg):
+        self.colour = msg.data.strip().lower()
+        print(f"{self.name}: Detected colour: {self.colour}")
+
+    def _stop_detector(self):
+        if self.process:            
+            try:
+                os.killpg(os.getpgid(self.process.pid), signal.SIGINT)  #send Ctrl+C to the process group
+                self.process.wait(timeout=5)
+            except subprocess.TimeoutExpired:
+                print(f"{self.name}: Colour detector did not stop in time, killing...")
+                os.killpg(os.getpgid(self.process.pid), signal.SIGKILL)
+            print(f"{self.name}: Colour detector stopped after loading")
+            self.process = None
+
+class OffLoading(py_trees.behaviour.Behaviour):
+    def __init__(self, name="OffLoading"):
         super().__init__(name)
         self.executed = False
+        self.node = None
+        self.servo_pub = None
+        self.blackboard = Blackboard()
+
+    def setup(self, **kwargs):
+        node = kwargs.get("node")
+        if node:
+            self.node = node
+            self.servo_pub = node.create_publisher(Float64MultiArray, "/servo_controller/commands", 10)
+            print(f"{self.name}: Ready to publish to servo")
 
     def update(self):
-        if not self.executed:
-            print("hi")
+        if self.executed:
+            return py_trees.common.Status.SUCCESS
+
+        # Get colour detected during loading
+        # loading_colour = self.blackboard.loading_colour
+
+        # Infer expected colour from waypoint name (e.g. "offloading_blue")
+        expected_colour = None
+        if "blue" in self.name.lower():
+            expected_colour = "blue"
+        elif "red" in self.name.lower():
+            expected_colour = "red"
+
+        if loaded_colour is None:
+            print(f"{self.name}: No loading colour stored, skipping offloading")
             self.executed = True
+            return py_trees.common.Status.SUCCESS
+
+        if expected_colour and loaded_colour == expected_colour:
+            print(f"{self.name}: Match found ({loaded_colour}), opening servo...")
+            self._publish_servo(90.0)  # OPEN
+            time.sleep(10)  # keep open
+            print(f"{self.name}: Closing servo after 20s")
+            self._publish_servo(170.0)  # CLOSE
+        else:
+            print(f"{self.name}: Mismatch (loading={loaded_colour}, expected={expected_colour}), no action")
+
+        self.executed = True
+        return py_trees.common.Status.SUCCESS
+
+    def _publish_servo(self, value):
+        msg = Float64MultiArray()
+        msg.data = [value]
+        self.servo_pub.publish(msg)
+        print(f"{self.name}: Servo set to {value}")
+
+class Potato_Detector(py_trees.behaviour.Behaviour):
+    def __init__(self, name="Potato_Detector"):
+        super().__init__(name)
+        self.process = None
+        self.start_time = None
+        self.timeout = 15  
+        self.phase = "start"
+
+    def update(self):
+        if self.phase == "start":
+            launch_cmd = [
+                "ros2", "launch",
+                "rdj2025_potato_disease_detection",   # package
+                "potato_detection.launch.py"          # launch file
+            ]
+
+            try:
+                self.process = subprocess.Popen(launch_cmd, preexec_fn=os.setsid) #start new process group
+                print(f"{self.name}: Started potato detection launch file")
+            except FileNotFoundError:
+                print(f"{self.name}: ros2 launch not found")
+                return py_trees.common.Status.FAILURE
+
+            self.start_time = time.time()
+            self.phase = "running"
+            return py_trees.common.Status.RUNNING
+
+        elif self.phase == "running":
+            if time.time() - self.start_time < self.timeout:
+                return py_trees.common.Status.RUNNING
+            else:
+                self.phase = "stopping"
+                return py_trees.common.Status.RUNNING
+
+        elif self.phase == "stopping":
+            if self.process:
+                
+                try:
+                    os.killpg(os.getpgid(self.process.pid), signal.SIGINT)  #send Ctrl+C to the process group
+                    self.process.wait(timeout=5)
+                    print(f"{self.name}: Potato detection stopped after {self.timeout}s")
+                except subprocess.TimeoutExpired:
+                    print(f"{self.name}: Potato detection did not stop in time, killing...")
+                    os.killpg(os.getpgid(self.process.pid), signal.SIGKILL)
+            self.phase = "done"
+            return py_trees.common.Status.SUCCESS
+
         return py_trees.common.Status.SUCCESS
 
 
@@ -155,13 +321,19 @@ def create_root():
             g["y"],
             g.get("yaw", 0.0)
         )
+        
         children.append(move)
 
         # Example: add a hello/hi task after each move
-        if "hello" in g["name"].lower():
-            children.append(PrintHello(f"Task_after_{g['name']}"))
+        if "potato" in g["name"].lower():
+            children.append(Potato_Detector(f"Image_detection_in_{g['name']}"))
+        elif "loading_zone" in g["name"].lower():
+            children.append(Loading(f"Loading_in_{g['name']}"))
+        elif "offloading" in g["name"].lower():
+            children.append(OffLoading(f"Offloading_in__{g['name']}"))
         else:
-            children.append(PrintHi(f"Task_after_{g['name']}"))
+            #children.append(Waypoint(f"Reached_{g['name']}"))
+            print(f"Reached_{g['name']}")
 
     root.add_children(children)
     return root
@@ -185,10 +357,10 @@ def main():
             if tree_completed:
                 return
             node.tick_tock(period_ms=100)
-            for i, child in enumerate(node.root.children):
-                if child.status in (py_trees.common.Status.SUCCESS,
-                                    py_trees.common.Status.RUNNING):
-                    print(f"  Child {i} ({child.name}): {child.status.name}")
+            # for i, child in enumerate(node.root.children):
+            #     if child.status in (py_trees.common.Status.SUCCESS,
+            #                         py_trees.common.Status.RUNNING):
+            #         print(f"  Child {i} ({child.name}): {child.status.name}")
             if node.root.status == py_trees.common.Status.SUCCESS:
                 print("Behavior tree completed successfully!")
                 tree_completed = True
@@ -211,3 +383,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# 
