e224487ad2660486f89c3f3a733a6822ce9c9b8e
Gareth Kipkoech <garethkip@gmail.com>
2025-10-02 19:22:20 +0300

    Creating potato disease detector launch file

---
+++ pi-code/src/rdj2025_potato_disease_detection/launch/potato_detection.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rdj2025_potato_disease_detection',
+            executable='publish_test_image',
+            name='image_pub',
+            output='screen',
+            # parameters=[{'IP': '10.122.180.67'}] #b - Fundi
+            parameters=[{'IP': '10.226.56.67'}] #Kabbage - Gareth
+            # parameters=[{'IP': '192.168.0.112'}]# Gamefield
+        ),
+        Node(
+            package='rdj2025_potato_disease_detection',
+            executable='potato_disease_detection_node',
+            name='disease_detector',
+            output='screen'
+        )
+    ])
